84347fc70512b228a1a72e5d6e2a4fbfccae1d3a
HarshPatani <harshpatani1923@gmail.com>
2023-03-28 13:36:42 +0530

    Preparing to deploy

---
+++ Models/UserModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Users", userSchema, "users");
